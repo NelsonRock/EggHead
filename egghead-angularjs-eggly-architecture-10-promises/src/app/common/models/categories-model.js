72e7bdfd63997c9e2e2598b465e758017b8dbac0
mikey-pawell <mikeypawell@gmail.com>
2016-01-21 15:42:32 -0800

    edit and create bookmark states

--- egghead-angularjs-eggly-architecture-10-promises/src/app/common/models/categories-model.js
+++ egghead-angularjs-eggly-architecture-10-promises/src/app/common/models/categories-model.js
@@ -1,7 +1,7 @@
 angular.module('eggly.models.categories', [
 
 ])
-    .service('CategoriesModel', function ($http, $q) {
+    .service('CategoriesModel', function ($http, $q) { //inject the q service
         var model = this,
             URLS = {
                 FETCH: 'data/categories.json'
@@ -17,21 +17,30 @@ angular.module('eggly.models.categories', [
             return categories;
         }
 
+        //q.when takes an object and it wraps a promise around it, in the promise, put the categories
+
         model.getCategories = function() {
             return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
         };
 
+        //q.defer returns a deffered object
+        //
+
         model.getCategoryByName = function(categoryName) {
             var deferred = $q.defer();
 
+
+            //convenince method that loops over the categories collection and compare the name of the current
+            //category with the parameter  
             function findCategory(){
                 return _.find(categories, function(c){
                     return c.name == categoryName;
-                })
+                });
             }
-
+            //if categories exist, resolve the promise with the result of findCategory
             if(categories) {
                 deferred.resolve(findCategory());
+            //if they don't exist, then you need to call getCategories(), and take the result to resolve the promise
             } else {
                 model.getCategories()
                     .then(function() {
@@ -40,6 +49,7 @@ angular.module('eggly.models.categories', [
             }
 
             return deferred.promise;
+            //return the promise 
         };
 
 
